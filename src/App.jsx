93e89b86eb1deb1044e86c96ef298057e54dda17
MrHacker10100 <ahsansalamat10100@gmail.com>
2025-07-10 14:26:23 +0500

    variables set

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-
+import Navbar from "./compponents/Navbar/Navbar.jsx"
 function App() {
   return (
-    <div>
-      <div className="bg-primary text-white">Primary</div>
-
-      <p className="text-accent-700">Sale Badge</p>
-      <div className="border border-neutral-300 p-4">Border Color</div>
-      <button className="bg-primary hover:bg-primary-700">Buy Now</button>
-    </div>
+    <>
+    <Navbar/>
+    </>
   );
 }
 
